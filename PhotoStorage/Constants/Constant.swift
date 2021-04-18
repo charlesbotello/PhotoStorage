ebba89f26a152bb5febfe6f6269256e8b94ba03e
charlesbotello <charlesabotello@gmail.com>
2021-04-18 17:22:17 -0500

    add list images and description

---
+++ PhotoStorage/Constants/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  PhotoStorage
+//
+//  Created by Charlie Botello on 4/18/21.
+//
+
+import Foundation
+
+class Constant {
+    static let imageCell: String = "ImageCell"
+}
---
+++ PhotoStorage/Controllers/PhotoListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  PhotoStorage
+//
+//  Created by Charlie Botello on 4/18/21.
+//
+
+import UIKit
+
+class PhotoListViewController: UITableViewController {
+
+//    var imagesArray: [UIImage] = ["BlueLogo.png", "CreamLogo.png", "WhiteLogo.png"]
+    var imagesArray: [String] = ["BlueLogo", "CreamLogo", "WhiteLogo"]
+    var imageText: [String] = ["Blue", "cream", "White"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+                    }
+
+
+    //MARK: - Tableviw Datasource Method
+    // Return the number of rows for the table.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imagesArray.count
+    }
+    
+    
+    // Provide a cell object for each row.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell
+        
+        cell = tableView.dequeueReusableCell(withIdentifier: Constant.imageCell)! as UITableViewCell
+        
+//        cell.imageView?.frame = CGRect(x: 300, y: 52, width: 292, height: 136)
+//        cell.imageView?.clipsToBounds = true
+//        cell.contentView.addSubview(cell.imageView!)
+        let image: UIImage = UIImage(named: imagesArray[indexPath.row])!
+        cell.imageView?.image = image
+        cell.textLabel?.text = imageText[indexPath.row]
+        return cell
+        
+    }
+}
+
